2c1217b05082c38d361155b738d33c3631c97983
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2021-01-30 20:38:18 -0500

    User register form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Register = () => {
+
+    const [email, setEmail] = useState('')
+    const [loading, setLoading] = useState('')
+
+    const handleSubmit = () => {
+        //
+    }
+
     return (
         <div className="container p-5">
-            <div className="row">
                 <h4>Register</h4>
-            </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>
+                            Email Addres
+                        </label>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Enter email" disabled={loading}/>
+                    </div>
+                    <button className="btn btn-raised btn-primary" disabled={!email || loading}>
+                        Submit
+                    </button>
+                </form>
         </div>
     )
 }
